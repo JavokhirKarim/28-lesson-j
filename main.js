d17cf3b542dd44863ba3d5d6c260b2ac3034fb7c
unknown <javohirkarimiy@gmail.com>
2023-06-22 14:09:08 +0500

    changed

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ document.querySelector("#menu").addEventListener("click", () => {
 window.addEventListener("scroll", function () {
   toggleBacktop();
 });
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
